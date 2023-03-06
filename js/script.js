ff5d379a314ac96ed23148a977f8c578ebe45005
Agatha <agathastanchi@gmail.com>
2023-03-05 22:06:05 -0300

    subida

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const options = {
+    bottom: '500px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌗', // default: '🌝'
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
